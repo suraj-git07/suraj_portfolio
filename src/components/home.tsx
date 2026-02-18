f418fa5c85dee49c63240c9dd1616eb52b44050d
suraj-git07 <suraj844722@gmail.com>
2026-02-18 09:44:48 +0530

    leetcode added

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -89,15 +89,30 @@ const Home = () => {
             {/* Profile Section - Image and Name */}
             <div className="flex items-start gap-4 sm:flex-col sm:items-start">
               {/* Profile Picture */}
-              <Image
-                data-aos="fade-down"
-                data-aos-delay="100"
-                src={`/profilep.png`}
-                height={10000}
-                width={10000}
-                alt={"Profile Photo"}
-                className="border-4 rounded-full w-[90px] h-[90px] sm:w-[150px] sm:h-[150px] sm:mb-8 border-white dark:border-[#1a1a1a] bg-white dark:bg-[#1a1a1a] relative z-5 sm:ml-6 flex-shrink-0 object-cover"
-              ></Image>
+              <div className="relative" data-aos="fade-down" data-aos-delay="100">
+                <Image
+                  src={`/profilep.png`}
+                  height={10000}
+                  width={10000}
+                  alt={"Profile Photo"}
+                  className="border-4 rounded-full w-[90px] h-[90px] sm:w-[150px] sm:h-[150px] sm:mb-8 border-white dark:border-[#1a1a1a] bg-white dark:bg-[#1a1a1a] relative z-5 sm:ml-6 flex-shrink-0 object-cover"
+                />
+                {/* LeetCode Icon Overlay */}
+                <Link 
+                  href="https://leetcode.com/u/Suraj_Mishra/" 
+                  target="_blank"
+                  className="absolute bottom-0 right-0 sm:bottom-6 sm:right-4 w-[24px] h-[24px] sm:w-[36px] sm:h-[36px] bg-[#FFA116] hover:bg-[#FF8C00] rounded-full flex items-center justify-center border-2 border-white dark:border-[#1a1a1a] transition-all duration-200 hover:scale-110 z-10"
+                  aria-label="LeetCode Profile"
+                >
+                  <svg 
+                    className="w-[14px] h-[14px] sm:w-[22px] sm:h-[22px] fill-white" 
+                    viewBox="0 0 24 24" 
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M13.483 0a1.374 1.374 0 0 0-.961.438L7.116 6.226l-3.854 4.126a5.266 5.266 0 0 0-1.209 2.104 5.35 5.35 0 0 0-.125.513 5.527 5.527 0 0 0 .062 2.362 5.83 5.83 0 0 0 .349 1.017 5.938 5.938 0 0 0 1.271 1.818l4.277 4.193.039.038c2.248 2.165 5.852 2.133 8.063-.074l2.396-2.392c.54-.54.54-1.414.003-1.955a1.378 1.378 0 0 0-1.951-.003l-2.396 2.392a3.021 3.021 0 0 1-4.205.038l-.02-.019-4.276-4.193c-.652-.64-.972-1.469-.948-2.263a2.68 2.68 0 0 1 .066-.523 2.545 2.545 0 0 1 .619-1.164L9.13 8.114c1.058-1.134 3.204-1.27 4.43-.278l3.501 2.831c.593.48 1.461.387 1.94-.207a1.384 1.384 0 0 0-.207-1.943l-3.5-2.831c-.8-.647-1.766-1.045-2.774-1.202l2.015-2.158A1.384 1.384 0 0 0 13.483 0zm-2.866 12.815a1.38 1.38 0 0 0-1.38 1.382 1.38 1.38 0 0 0 1.38 1.382H20.79a1.38 1.38 0 0 0 1.38-1.382 1.38 1.38 0 0 0-1.38-1.382z"/>
+                  </svg>
+                </Link>
+              </div>
               
               {/* Name and Title */}
               <div data-aos="fade-right" className="flex-1 sm:w-full mt-4 sm:mt-0">
